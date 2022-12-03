aae3105cb2e643ad477efcba80810b24a3d384ba
Juan Estenan Patiño <esteban09code@gmail.com>
2022-12-03 13:08:44 -0500

    Se inicia la creacion de la primera vista

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { NgModel } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+
+
+const routes : Routes = [
+{
+    path : '',
+    pathMatch : 'full',
+    redirectTo : 'home'
+},
+{
+    path : 'home',
+    component : HomeComponent
+    
+}
+
+]
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRouterModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
